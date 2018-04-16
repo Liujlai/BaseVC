946482de98d743f424b062cd5013ff85e300f8e7
idea <idea@idea.local>
2018-04-16 15:22:30 +0800

    Initial Commit

---
+++ BaseVC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaseVC
+//
+//  Created by idea on 2018/4/16.
+//  Copyright © 2018年 idea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaseVC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaseVC
+//
+//  Created by idea on 2018/4/16.
+//  Copyright © 2018年 idea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
